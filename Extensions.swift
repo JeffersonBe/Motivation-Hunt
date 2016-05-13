8fd10fff7200e41530fc26b1c144f4a563829c59
Jeffersonbe <jefferson.bonnaire@gmail.com>
2016-05-13 21:26:31 +0100

    Fix: Assets and Colors extension

---
+++ Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Motivation Hunt
+//
+//  Created by Jefferson Bonnaire on 08/05/2016.
+//  Copyright © 2016 Jefferson Bonnaire. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static func blackTransparentColor() -> UIColor {
+        return UIColor(red:0.11, green:0.11, blue:0.11, alpha: 0.8)
+    }
+}
